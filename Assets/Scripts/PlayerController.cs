b6c6fe3d9fd4d2a38957beb956e11f0e65a669c0
thirtytwoten <bj@lawn-128-61-23-111.lawn.gatech.edu>
2016-06-15 13:27:20 -0400

    create basic playing field with player and controlls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Animator anim;
+	private CharacterController charController;
+	private float speed = 6.0f;
+	private float gravity = -9.8f;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		charController = GetComponent<CharacterController> ();
+	}
+
+	void Update () {
+		float groundDistance = Mathf.Abs (transform.position.y);
+		float deltaZ = Input.GetAxis ("Vertical") * speed;
+		anim.SetFloat ("forward", deltaZ);
+		Vector3 movement = new Vector3 (0, 0, deltaZ);
+		movement = Vector3.ClampMagnitude (movement, speed);
+		movement.y = gravity;
+		movement *= Time.deltaTime;
+		movement = transform.TransformDirection (movement);
+		charController.Move (movement);
+	}
+}
