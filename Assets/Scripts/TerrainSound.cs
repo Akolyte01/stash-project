9c8e096d00fb66b7ab56ac0679dcb13e7c6131be
KaiserNinja <jhsu44@gatech.edu>
2016-06-24 21:26:18 -0400

    Additional work on my level

---
+++ Assets/Scripts/TerrainSound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainSound : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+}
