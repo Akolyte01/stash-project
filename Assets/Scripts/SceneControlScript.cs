3e245c56a7e20815b0aefda0418a97e384881703
Stephanie <stephanie.n.dykes@gmail.com>
2016-07-07 16:45:21 -0400

    Remove all scenes except for default scene, adjust SceneControlScript accordingly

--- Assets/Scripts/SceneControlScript.cs
+++ Assets/Scripts/SceneControlScript.cs
@@ -17,18 +17,18 @@ public class SceneControlScript : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Alpha0)) {
 			Application.LoadLevel (0); //this index is determined by the order scenes were added to build settings (File -> Build Settings...)
 		}
-		if (Input.GetKeyDown (KeyCode.Alpha1)) {
-			Application.LoadLevel (1);
-		}
-		if (Input.GetKeyDown (KeyCode.Alpha2)) {
-			Application.LoadLevel (2);
-		}
-		if (Input.GetKeyDown (KeyCode.Alpha3)) {
-			Application.LoadLevel (3);
-		}
-		if (Input.GetKeyDown (KeyCode.Alpha4)) {
-			Application.LoadLevel (4);
-		}
+//		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+//			Application.LoadLevel (1);
+//		}
+//		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+//			Application.LoadLevel (2);
+//		}
+//		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+//			Application.LoadLevel (3);
+//		}
+//		if (Input.GetKeyDown (KeyCode.Alpha4)) {
+//			Application.LoadLevel (4);
+//		}
 
 	}
 }
