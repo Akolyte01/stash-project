fd75399742555b7770a90f92d5a4f1eb59682a64
olahay3 <olahay3@gatech.edu>
2016-07-20 20:49:30 -0400

    Added UI for start menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+    public Canvas hud;
+
+	// Use this for initialization
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        hud = hud.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+        hud.enabled = false;
+	}
+	
+	public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+
+        SceneManager.LoadScene("DefaultScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
